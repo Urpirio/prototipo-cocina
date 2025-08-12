eb814538b21193d9d3240429ccf298ac966e4047
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-08-11 22:16:15 -0400

    Se agrego el  Data Receta del HOme

---
+++ Components/Componentes_Home/Data/Data_Recetas.ts
@@ -0,0 +1,52 @@
+import BolasCarne from "@/../public/Img/Recetas/BolasCarne.svg";
+import Burger from "@/../public/Img/Recetas/Burger.svg";
+import ComidaFresh from "@/../public/Img/Recetas/ComidaFresh.svg";
+import ComidaNaraja from "@/../public/Img/Recetas/ComidaNaraja.svg";
+import Panquei from "@/../public/Img/Recetas/Panquei.svg";
+import Salmon from "@/../public/Img/Recetas/Salmon.svg";
+
+
+export const Data_Recetas = [
+  {
+    key:1,
+    titulo: "Big and Juicy Wagyu Beef Cheeseburger",
+    tiempo: "30 Minutes",
+    tipo: "Snack",
+    image: Burger,
+  },
+  {
+    key:2,
+    titulo: "Fresh Lime Roasted Salmon with Ginger Sauce",
+    tiempo: "30 Minutes",
+    tipo: "Fish",
+    image: Salmon,
+  },
+  {
+    key:3,
+    titulo: "Strawberry Oatmeal Pancake with Honey Syrup",
+    tiempo: "30 Minutes",
+    tipo: "Breakfast",
+    image: Panquei,
+  },
+  {
+    key:4,
+    titulo: "Fresh and Healthy Mixed Mayonnaise Salad",
+    tiempo: "30 Minutes",
+    tipo: "Healthy",
+    image: ComidaFresh,
+  },
+  {
+    key:5,
+    titulo: "Chicken Meatballs with Cream Cheese",
+    tiempo: "30 Minutes",
+    tipo: "Meat",
+    image: ComidaNaraja,
+  },
+  {
+    key:6,
+    titulo: "Fruity Pancake with Orange & Blueberry",
+    tiempo: "30 Minutes",
+    tipo: "Sweet",
+    image: BolasCarne,
+  },
+];
