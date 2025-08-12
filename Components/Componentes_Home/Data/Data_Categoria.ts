60ac299663f1b0567767b05bc37500af69fd00d1
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-08-11 22:16:02 -0400

    Se agrego el array de datos de categoria

---
+++ Components/Componentes_Home/Data/Data_Categoria.ts
@@ -0,0 +1,39 @@
+import Cake from "@/../public/Img/Categoria/Cake.svg";
+import Carne from "@/../public/Img/Categoria/Carne.svg";
+import Japon from "@/../public/Img/Categoria/Japon.svg";
+import Pan from "@/../public/Img/Categoria/Pan.svg";
+import Vegan from "@/../public/Img/Categoria/Vegan.svg";
+import Choco from "@/../public/Img/Categoria/Choco.svg";
+
+export const Data_Categoria = [
+  {
+    image: Japon,
+    Titulo: "Breakfast",
+    Bg: "to-white/10 via-white/60  from-[#708246]/30",
+  },
+  {
+    image: Vegan,
+    Titulo: "Vegan",
+    Bg: "to-white/10 via-white/60  from-[#6CC63F]/30",
+  },
+  {
+    image: Carne,
+    Titulo: "Meat",
+    Bg: "to-white/10 via-white/60  from-[#CC261B]/30",
+  },
+  {
+    image: Cake,
+    Titulo: "Dessert",
+    Bg: "to-white/10 via-white/60  from-[#F09E00]/30",
+  },
+  {
+    image: Pan,
+    Titulo: "Lunch",
+    Bg: "to-white/10 via-white/60  from-[#708246]/10",
+  },
+  {
+    image: Choco,
+    Titulo: "Chocolate",
+    Bg: "to-white/10 via-white/60  from-[#708246]/10",
+  },
+];
