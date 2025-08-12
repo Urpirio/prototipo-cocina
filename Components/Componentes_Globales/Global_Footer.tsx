6b6e498422889cb62e20dc5a3f32c42ec8b52cd9
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-08-11 22:14:15 -0400

    Se agrego la fase 1 del footer

---
+++ Components/Componentes_Globales/Global_Footer.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+import Link from "next/link";
+import Logo from "@/../public/Img/Foodieland..svg"
+
+
+export default function Global_Footer() {
+  return (
+    <footer className="py-10 border">
+        <section className="flex  justify-between items-center px-5">
+          <div className="flex flex-grow flex-col">
+            <Image src={Logo}  alt=""/>
+            <h2>Lorem ipsum dolor sit amet, consectetuipisicing elit.</h2>
+          </div>
+          <nav className="flex flex-grow justify-end gap-5">
+            <Link href={""}>Recipes</Link>
+            <Link href={""}>Blog</Link>
+            <Link href={""}>Contact</Link>
+            <Link href={""}>About us</Link>
+          </nav>
+        </section>
+        <section>
+          <div>
+            <h3></h3>
+          </div>
+        </section>
+    </footer>
+  )
+}
