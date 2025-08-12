af6d5c907afe588f60dab9992ec27c960a1f6ae1
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-08-12 15:20:35 -0400

    Se le hicieron mejoras al header

--- Components/Componentes_Globales/Global_Header.tsx
+++ Components/Componentes_Globales/Global_Header.tsx
@@ -9,18 +9,18 @@ import { FaInstagram } from "react-icons/fa";
 
 export default function Global_Header() {
   return (
-    <header className="flex items-center border justify-between px-5 py-8 ">
-      <div className="border flex flex-grow">
+    <header className="flex items-center border-b border-gray-300 justify-between px-5 py-8 ">
+      <div className=" flex flex-grow">
         <Image src={Logo} alt="Logo-Company" />
       </div>
-      <nav className="flex gap-5 justify-center border flex-grow">
-        <Link href={"/"}>Home</Link>
-        <Link href={"/recipes"}>Recipes</Link>
-        <Link href={"/blog"}>Blog</Link>
-        <Link href={"/contact"}>Contact</Link>
-        <Link href={"/about_us"}>About us</Link>
+      <nav className="flex gap-10 justify-center  flex-grow">
+        <Link className="font-semibold" href={"/"}>Home</Link>
+        <Link className="font-semibold" href={"/recipes"}>Recipes</Link>
+        <Link className="font-semibold" href={"/blog"}>Blog</Link>
+        <Link className="font-semibold" href={"/contact"}>Contact</Link>
+        <Link className="font-semibold" href={"/about_us"}>About us</Link>
       </nav>
-      <div className="flex flex-grow justify-end items-center gap-5 text-xl border">
+      <div className="flex flex-grow justify-end items-center gap-5 text-xl ">
         <Link href={""}>
           <LiaFacebookF />
         </Link>
